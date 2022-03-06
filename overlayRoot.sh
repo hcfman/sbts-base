9b5129cf6750a64c4f055e6392a3167d5f0350dd
Kim Hendrikse <kim@kiwiclan.com>
2022-03-06 08:58:11 +0100

    rmdir straggling newroot directory

--- overlayRoot.sh
+++ overlayRoot.sh
@@ -31,12 +31,12 @@ if [ ! -e /sys/fs ] ; then
     mount -t sysfs sysfs /sysfs
 fi
 
-if [ "$found_init" != "/sbin/overlayRoot.sh" -a ! -z "$found_sbtsroot" ] ; then
-    if [ -d "/mnt/newroot" ] ; then
-        # Extra resilience
-        rmdir /mnt/newroot
-    fi
+if [ -d "/mnt/newroot" ] ; then
+    # Extra resilience
+    rmdir /mnt/newroot
+fi
 
+if [ "$found_init" != "/sbin/overlayRoot.sh" -a ! -z "$found_sbtsroot" ] ; then
     mkdir /mnt/newroot || fail "Can't create /mnt/newroot"
 
     mount $found_sbtsroot /mnt/newroot || fail "Can't mount $found_sbtsroot on /mnt"
